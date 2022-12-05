bd55ba8903dbcf417068ce2b23ffe160dc413b31
Oleg_bul <kokseg63@gmail.com>
2022-12-05 19:47:19 +0400

    task 5 added

--- Home_work_4_5.py
+++ Home_work_4_5.py
@@ -6,3 +6,15 @@
 # 4*x**2 + 1*x + 4
 # Результирующий файл:
 # 6*x**2 + 5*x + 9
+import sympy
+
+with open('file1.txt', 'r') as f:
+    a = f.read()
+with open('file2.txt', 'r') as f:
+    b = f.read()
+print('(', a, ')', ' + ', '(', b, ')')
+
+ans = sympy.sympify(a+b)
+with open('file3.txt', 'a') as f:
+    f.write(str(ans))
+f.close
