2931b8857be1d01edefd8fc13e8c5cf8e385f100
Oleg-Bul <112970479+Oleg-Bul@users.noreply.github.com>
2022-12-06 21:14:15 +0400

    Teacher task 2 added

--- Home_work_4_2.py
+++ Home_work_4_2.py
@@ -30,3 +30,24 @@ def RemoveRepeat(nums: list) -> list:
 
 
 print(RemoveRepeat(PrimeFactors(x)))
+
+#Решение от преподователя
+def dividers(a: int, uniq: bool = False) -> list[int]:
+    """"Возвращает список простых делителей числа.
+    uniq = True вернет список уникальных делителей"""
+    i = 2
+    dividers = []
+    while a != 1:
+        while a % i == 0:
+            dividers.append(i)
+            a //= i
+        i += 1
+    if uniq:
+        return list(set(dividers))
+    else:
+        return dividers
+
+
+a = 81
+print(f'Список натуральных делителей числа {a}:{dividers(a)}')
+print(f'Список уникальных делителей числа {a}:{dividers(a, True)}')
