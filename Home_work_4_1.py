dd8d180665d37e8b1d44724e473f114bec37b745
Oleg-Bul <112970479+Oleg-Bul@users.noreply.github.com>
2022-12-06 21:08:26 +0400

    Teacher task 1 added

--- Home_work_4_1.py
+++ Home_work_4_1.py
@@ -8,3 +8,19 @@ import math
 d = str(input('Введите точночть "d": '))
 count = len(str(d)) - 2
 print('Точность ', count, 'знаков после запятой = ', round(math.pi, count))
+
+
+# Решение от преподователя
+
+from math import pi
+
+
+def format_by_mask(num: float, accuracy: float) -> float:
+    """"форматирует число по заданной маске"""
+    accuracy = str(accuracy)
+    accuracy = len(accuracy[accuracy.find('.')+1::])
+    return float(f'{pi:0.{accuracy}f}')    # f'a:0.3f'
+
+
+d = input('Введите точность в формате "0.001": ')
+print(format_by_mask(pi, d))
